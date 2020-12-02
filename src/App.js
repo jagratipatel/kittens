b478242a04e65b200e205db3cdc5ff60648023b9
Jagrati <pateljagrati18@gmail.com>
2020-12-02 20:44:38 +0530

    Kittens_addeded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+/* import './App.css';
+import React, { useState , useEffect} from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then((response) => response.json())
+  .then((json) =>  setData(json))
+
+    const url = 'https://robohash.org/set_set4/bgset_bg1/3.14159?size=300x300'
+
+  fetch(url)
+  .then((response) => response.json())
+  .then((json) =>  console.log(json)) 
+
+  },[data,setData])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          Kitten's {data.title}
     </div>
   );
 }
 
+export default App; */
+
+import './App.css';
+import React, { Component } from 'react';
+import Card from './components/card'
+class App extends Component {
+  state = {
+    users:[]
+  }
+  componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((json) =>  this.setState({
+    users : json
+  })) 
+/*   Promise.all([
+    fetch("https://jsonplaceholder.typicode.com/users"),
+    fetch("https://robohash.org/139.162.116.133.png"),
+  ]).then(([res1, res2]) => {
+    this.setState({status: "fetched"})
+  }) */
+  }
+  
+render(){
+  return(
+      <div id="back">   
+      {this.state.users.map(user=><Card key= {user.id} name={user.name} id={user.id}  />)} 
+      </div>    
+  )
+}
+}
+
+
+ 
 export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './card.css'
+
+class Card extends Component {
+    
+    render() { 
+        return (
+         <div id="card">
+            <div id="head">
+                <h1>{this.props.name}</h1>
+                <h3>Lorem ipsum dolor sit.</h3>
+                <img src={`https://robohash.org/set_set4/13${this.props.id}.162.116.133.png`} alt="#"/>
+            </div>   
+         </div> );
+    }
+}
+ 
+export default Card 
