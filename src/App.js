6c610df59be6f95dfe55847d6802f1cc502a9669
Jagrati <pateljagrati18@gmail.com>
2020-12-02 22:26:18 +0530

    change in style

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-/* import './App.css';
-import React, { useState , useEffect} from 'react';
-
-function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-  fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then((response) => response.json())
-  .then((json) =>  setData(json))
-
-    const url = 'https://robohash.org/set_set4/bgset_bg1/3.14159?size=300x300'
-
-  fetch(url)
-  .then((response) => response.json())
-  .then((json) =>  console.log(json)) 
-
-  },[data,setData])
-  return (
-    <div className="App">
-          Kitten's {data.title}
-    </div>
-  );
-}
-
-export default App; */
-
 import './App.css';
 import React, { Component } from 'react';
 import Card from './components/card'
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -6,11 +6,8 @@ class Card extends Component {
     render() { 
         return (
          <div id="card">
-            <div id="head">
-                <h1>{this.props.name}</h1>{/* 
-                <h3>Lorem ipsum dolor sit.</h3> */}
+                <h1>{this.props.name}</h1>
                 <img src={`https://robohash.org/set_set4/13${this.props.id}.162.116.133.png`} alt="#"/>
-            </div>   
          </div> );
     }
 }
